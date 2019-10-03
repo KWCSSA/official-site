fa134d576baabc46878fe93e6066304a1aee781a
Tianyu Wang <terrywang@tianyu.ninja>
2019-10-03 15:55:42 -0400

    created api for banners

--- server/routes/eventRoutes.js
+++ server/routes/eventRoutes.js
@@ -2,10 +2,16 @@ const fs = require('fs');
 const path = require('path');
 
 const eventDataFilePath = path.join(__dirname, '../data/events.json');
+const bannerDataFilePath = path.join(__dirname, '../data/eventBanners.json');
 
 module.exports = app => {
   app.get('/api/event/list', async (req, res) => {
     var eventList = JSON.parse(await fs.readFileSync(eventDataFilePath));
     res.send(eventList);
+  });
+
+  app.get('/api/event/banners', async (req, res) => {
+    var banners = JSON.parse(await fs.readFileSync(bannerDataFilePath));
+    res.send(banners);
   });
 };
