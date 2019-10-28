3460e5451bf2f6c26b2237334e2ca0a2d97de352
Tianyu Wang <terrywang@tianyu.ninja>
2019-10-28 14:24:58 -0400

    added react-reveal for enter effect

--- client/src/components/home/HighlightEventCard.jsx
+++ client/src/components/home/HighlightEventCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Fade from 'react-reveal/Fade';
 
 import '../../css/home/highlightEventCard.css';
 
@@ -8,19 +9,21 @@ function cardOnClick(link) {
 
 export default ({ event }) => {
 	return (
-		<div className='card shadow border' onClick={() => cardOnClick(event.link)}>
-			<div className='card-img-top card-img'>
-				<img className='card-pic' src={event.pic} alt={`${event.title} pic`} />
-			</div>
-			<div className='card-body'>
-				<h3 className='card-title'>{event.title}</h3>
-				<div className='card-text'>
-					<p>{event.content}</p>
+		<Fade bottom>
+			<div className='card shadow border' onClick={() => cardOnClick(event.link)}>
+				<div className='card-img-top card-img'>
+					<img className='card-pic' src={event.pic} alt={`${event.title} pic`} />
 				</div>
-				<div className='card-text'>
-					<small className='text-muted'>点击查看详情</small>
+				<div className='card-body'>
+					<h3 className='card-title'>{event.title}</h3>
+					<div className='card-text'>
+						<p>{event.content}</p>
+					</div>
+					<div className='card-text'>
+						<small className='text-muted'>点击查看详情</small>
+					</div>
 				</div>
 			</div>
-		</div>
+		</Fade>
 	);
 };
